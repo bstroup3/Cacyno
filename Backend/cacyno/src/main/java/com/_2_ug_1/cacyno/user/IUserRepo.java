5f3f173f21d2bde87001b16d1b5eab0e200345f9
Ben Stroup <56928392+bstroup3@users.noreply.github.com>
2022-12-20 17:03:23 -0600

    actual update

--- Backend/cacyno/src/main/java/com/_2_ug_1/cacyno/user/IUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com._2_ug_1.cacyno.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-import java.util.UUID;
-
-public interface IUserRepo extends JpaRepository<User, String> {
-}
